9d8c5ad69bc21b134c94567a532e97a5155c45ae
Apoorva <apoorvakarekar@gmail.com>
2024-09-05 21:21:51 +0200

    Write another approach using array methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,25 @@
-const palindromes = function (inputStr) {
-    const punctuation = `.!'", `;
-    const str = inputStr.toLowerCase();
-    const originalArray =[...str].filter((letter)=>!(punctuation.includes(letter)));
-    const reverseArray = originalArray.toReversed();
-    return originalArray.every((v,i) => v === reverseArray[i]);
+// const palindromes = function (inputStr) {
+//     const punctuation = `.!'", `;
+//     const str = inputStr.toLowerCase();
+//     const originalArray =[...str].filter((letter)=>!(punctuation.includes(letter)));
+//     const reverseArray = originalArray.toReversed();
+//     return originalArray.every((v,i) => v === reverseArray[i]);
+// };
 
-};
+
+//Another option
+const punctuation = `.!'", `;
+const palindromes = function(inputStr) {
+    const cleanedString = inputStr.split("")
+                   .filter(letter => !(punctuation.includes(letter)))
+                   .map(letter => letter.toLowerCase());
+
+    const reversedString = cleanedString.toReversed();
+
+    return cleanedString.every((letter,index) => 
+                letter === reversedString[index]);
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
